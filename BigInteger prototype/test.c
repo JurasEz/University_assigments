e5cb844449b78d515b0452f55a47c407da2f0697
Juras Ezerskis <115423590+JurasEz@users.noreply.github.com>
2023-02-23 21:53:35 +0200

    Update and rename old to test.c

---
+++ BigInteger prototype/test.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "functions.h"
+
+int main() {
+    BigInt num1, num2;
+    BigInt sum, diff, mul, div;
+
+    printf("Enter the first Integer:");
+    getLongInt(&num1);
+    printf("Enter the first Integer:");
+    getLongInt(&num2);
+
+    sum = addLongInt(num1, num2);
+    printf("Sum Answer = ");
+    printMyLongInt(sum);
+
+    diff = subLongInt(num1, num2);
+    printf("Difference Answer = ");
+    printMyLongInt(diff);
+
+    mul = mulLongInt(num1, num2);
+    printf("Multiplication Answer = ");
+    printMyLongInt(mul);
+
+    div = divLongInt(num1, num2);
+    printf("Division Answer = ");
+    printMyLongInt(div);
+
+    return 0;
+}
