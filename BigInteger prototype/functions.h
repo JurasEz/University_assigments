8b152eb3f1c9d9c4731b88cbf71fc4f4c3882aa4
Juras Ezerskis <115423590+JurasEz@users.noreply.github.com>
2023-03-02 21:39:07 +0200

    Update functions.h

--- BigInteger prototype/functions.h
+++ BigInteger prototype/functions.h
@@ -7,15 +7,15 @@ typedef struct BigInt {
     int length;
 } BigInt;
 
-extern void getLongInt(BigInt *number);
-extern void printMyLongInt(BigInt num);
-extern BigInt addLongInt(BigInt num1, BigInt num2);
-extern BigInt subLongInt(BigInt content, BigInt sub);
-extern BigInt subPositiveLongInts(BigInt larger, BigInt smaller, int maxLen);
+extern void getBigInt(BigInt *number);
+extern void printMyBigInt(BigInt num);
+extern BigInt addBigInt(BigInt num1, BigInt num2);
+extern BigInt subBigInt(BigInt content, BigInt sub);
+extern BigInt subPositiveBigInts(BigInt larger, BigInt smaller, int maxLen);
 extern bool getAnswerSign(BigInt content, BigInt sub, int maxLen);
-extern BigInt mulLongInt(BigInt num1, BigInt num2);
-extern BigInt divLongInt(BigInt dividend, BigInt divisor);
-extern int compareLongInts(BigInt num1, BigInt num2);
+extern BigInt mulBigInt(BigInt num1, BigInt num2);
+extern BigInt divBigInt(BigInt dividend, BigInt divisor);
+extern int compareBigInts(BigInt num1, BigInt num2);
 extern bool isEmpty(BigInt num);
 extern BigInt clone(BigInt num);
 
