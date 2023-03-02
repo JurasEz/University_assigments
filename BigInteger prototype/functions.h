b813e39f2821e1c6d72901c36653b6d370713d85
Juras Ezerskis <115423590+JurasEz@users.noreply.github.com>
2023-03-02 22:19:16 +0200

    Update functions.h

--- BigInteger prototype/functions.h
+++ BigInteger prototype/functions.h
@@ -12,7 +12,7 @@ extern void printMyBigInt(BigInt num);
 extern BigInt addBigInt(BigInt num1, BigInt num2);
 extern BigInt subBigInt(BigInt content, BigInt sub);
 extern BigInt subPositiveBigInts(BigInt larger, BigInt smaller, int maxLen);
-extern bool getAnswerSign(BigInt content, BigInt sub, int maxLen);
+extern bool compareSign(BigInt content, BigInt sub, int maxLen);
 extern BigInt mulBigInt(BigInt num1, BigInt num2);
 extern BigInt divBigInt(BigInt dividend, BigInt divisor);
 extern int compareBigInts(BigInt num1, BigInt num2);
